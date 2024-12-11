4e3fd0a04823dd4f1bc99b0420753b3a87646812
HansonWbee <1452936-x@g.puiching.edu.mo>
2024-12-11 11:53:04 +0800

    spawn

---
+++ S2E animaltower/Assets/script/AnimalCollect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalCollect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Something's Here");
+        
+        bool isAnimal = false;
+        isAnimal = collision.gameObject.CompareTag("Animal");
+        if(isAnimal)
+        {
+            Debug.Log("It's an Animal!!");
+            Destroy(collision.gameObject);
+            Debug.Log("Game Over!!");
+        }
+    }
+
+}
